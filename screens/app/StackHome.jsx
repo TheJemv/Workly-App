b6eaa4f15302bd6ea870dcdeda071a307b457c15
Harold Challco <yhon53190@gmail.com>
2024-09-12 15:15:30 -0500

    Created 1 screen design and functionality for profile company with links social media, call phone, list services, list times open and info company

--- screens/app/StackHome.jsx
+++ screens/app/StackHome.jsx
@@ -1,43 +1,53 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-import NativeStackOptions from "@/constants/NativeStackOptions"
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import NativeStackOptions from "@/constants/NativeStackOptions";
 
-import { HomeServicesData } from "data"
+import { HomeServicesData } from "data";
 
-import HomeScreen from "./home"
-import ServicesScreenCategory from "./home/ServicesCategory"
-import ServiceHire from "./home/ServiceHire"
+// import HomeScreen from "./home"
+import ServicesScreenCategory from "./home/ServicesCategory";
+import ServiceHire from "./home/ServiceHire";
+import { ProfileCompanyScreen } from "./profile-company";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 const StackHome = () => {
    return (
       <Stack.Navigator
          initialRouteName="home"
          screenOptions={NativeStackOptions}
       >
-         <Stack.Screen name="home" component={HomeScreen} />
+         <Stack.Screen
+            name="home"
+            component={ProfileCompanyScreen}
+            options={{
+               headerShown: false,
+            }}
+         />
          <Stack.Screen
             name="service"
             component={ServiceHire}
             options={{
-               headerTitle: "Contratar Servicio"
+               headerTitle: "Contratar Servicio",
             }}
          />
 
          <Stack.Group>
             {HomeServicesData.map((data) => (
                <Stack.Screen
-                  name={`${data.label}screen`} i
+                  name={`${data.label}screen`}
+                  i
                   initialParams={data}
                   component={ServicesScreenCategory}
                   options={{
-                     headerTitle: data.label.charAt(0).toUpperCase() + data.label.slice(1),
+                     headerTitle:
+                        data.label.charAt(0).toUpperCase() +
+                        data.label.slice(1),
                   }}
                   key={data.label}
                />
             ))}
          </Stack.Group>
       </Stack.Navigator>
-   )
-}
+   );
+};
 
-export default StackHome
+export default StackHome;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,38 @@
-const themes = require("./config/theme.json")
+const themes = require("./config/theme.json");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./components/**/*.{js,jsx,ts,tsx}",
-    "./screens/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        text: themes.colors.default.text_color.default,
-        light: themes.colors.default.text_color.light,
-        dark: themes.colors.default.text_color.dark,
-        primary: themes.colors.default.theme_color.primary,
-        secondary: themes.colors.default.theme_color.secondary,
-        body: themes.colors.default.theme_color.body,
-        border: themes.colors.default.theme_color.border,
-        "theme-light": themes.colors.default.theme_color.theme_light,
-        "theme-dark": themes.colors.default.theme_color.theme_dark,
-        darkmode: {
-          text: themes.colors.darkmode.text_color.default,
-          light: themes.colors.darkmode.text_color.light,
-          dark: themes.colors.darkmode.text_color.dark,
-          primary: themes.colors.darkmode.theme_color.primary,
-          secondary: themes.colors.darkmode.theme_color.secondary,
-          body: themes.colors.darkmode.theme_color.body,
-          border: themes.colors.darkmode.theme_color.border,
-          "theme-light": themes.colors.darkmode.theme_color.theme_light,
-          "theme-dark": themes.colors.darkmode.theme_color.theme_dark,
-        },
-      }
-    },
-  },
-  plugins: [],
-}
-
+   content: [
+      "./components/**/*.{js,jsx,ts,tsx}",
+      "./screens/**/*.{js,jsx,ts,tsx}",
+   ],
+   theme: {
+      extend: {
+         colors: {
+            text: themes.colors.default.text_color.default,
+            light: themes.colors.default.text_color.light,
+            dark: themes.colors.default.text_color.dark,
+            primary: themes.colors.default.theme_color.primary,
+            secondary: themes.colors.default.theme_color.secondary,
+            body: themes.colors.default.theme_color.body,
+            border: themes.colors.default.theme_color.border,
+            "theme-light": themes.colors.default.theme_color.theme_light,
+            "theme-dark": themes.colors.default.theme_color.theme_dark,
+            darkmode: {
+               text: themes.colors.darkmode.text_color.default,
+               light: themes.colors.darkmode.text_color.light,
+               dark: themes.colors.darkmode.text_color.dark,
+               primary: themes.colors.darkmode.theme_color.primary,
+               secondary: themes.colors.darkmode.theme_color.secondary,
+               body: themes.colors.darkmode.theme_color.body,
+               border: themes.colors.darkmode.theme_color.border,
+               "theme-light": themes.colors.darkmode.theme_color.theme_light,
+               "theme-dark": themes.colors.darkmode.theme_color.theme_dark,
+            },
+            yellow: "#ffd56f",
+            red: "#dc6666",
+         },
+      },
+   },
+   plugins: [],
+};
