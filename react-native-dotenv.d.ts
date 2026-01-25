b20b1f8a1e83f42e45eb161fca7da02dd220dea4
TheJemv <montemilio@hotmail.com>
2026-01-25 14:18:02 -0600

    Actualizacion de Firebase y .env

--- react-native-dotenv.d.ts
+++ react-native-dotenv.d.ts
@@ -1,5 +1,19 @@
 declare module "@env" {
+   // API
    export const API_HOST: string;
    export const API_WEBHOOK: string;
+
+   // Google
    export const GOOGLE_API_MAP: string;
+
+   // Stripe
+   export const PUBLISHABLE_KEY: string;
+
+   // Firebase
+   export const FIREBASE_API_KEY: string;
+   export const FIREBASE_AUTH_DOMAIN: string;
+   export const FIREBASE_PROJECT_ID: string;
+   export const FIREBASE_STORAGE_BUCKET: string;
+   export const FIREBASE_MESSAGING_SENDER_ID: string;
+   export const FIREBASE_APP_ID: string;
 }
